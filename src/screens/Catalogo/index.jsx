7df8ab2f4c9199e8efb0d55a57d00cf1457cae19
mquiss <mariaquispegamez@gmail.com>
2025-05-09 22:03:17 -0300

    feat(catalogo): implementación de componente de ordenamiento

--- src/screens/Catalogo/index.jsx
+++ src/screens/Catalogo/index.jsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
-import Carousel from "@src/components/Carousel.jsx";
 import { useProductos } from "@src/contexts/ProductContext.jsx";
 import SearchBar from "@src/components/SearchBar.jsx";
 import ProductCard from "@src/components/ProductCard.jsx";
+import SortedBy from "./components/sorted-by";
+import Carousel from "@src/components/Carousel.jsx";
+
+import {
+  ordenarPorPrecio,
+  ordenarPorTitulo,
+} from "../../utils/ordenarProductos";
 
 import {
   filtrarPorNombre,
@@ -12,6 +18,16 @@ import {
 export function Catalogo() {
   const { productos } = useProductos();
   const [productosFiltrados, setProductosFiltrados] = useState([]);
+  const [type, setType] = useState("title");
+  const [order, setOrder] = useState("asc");
+
+  const handleTypeData = (data) => {
+    setType(data);
+  };
+
+  const handleOrderData = (data) => {
+    setOrder(data);
+  };
 
   const handleBuscar = (termino) => {
     if (!termino) return setProductosFiltrados([]);
@@ -21,34 +37,49 @@ export function Catalogo() {
   const destacados = filtrarDestacados(productos);
   const lista = productosFiltrados.length ? productosFiltrados : productos;
 
+  let sortedLista = lista;
+
+  if (type === "title") {
+    sortedLista = ordenarPorTitulo(lista, order);
+  } else if (type === "price") {
+    sortedLista = ordenarPorPrecio(lista, order);
+  }
+
   return (
-    <>
+    <div className="p-10 lg:p-12">
       {/* Grid principal */}
 
-      <div className="grid grid-cols-1 md:grid-cols-[1fr_auto] items-center gap-4 p-12 bg-white">
-        <div>
-          <h1 className="text-xl text-center md:text-left text-brand-black uppercase font-bold">
+      <div className="grid grid-cols-1 md:grid-cols-[1fr_auto]  gap-4">
+        <div className="text-center md:text-left">
+          <h1 className="text-xl text-brand-black uppercase font-bold">
             Catálogo de productos
           </h1>
-          <h2 className="text-gray-500 text-center md:text-left">
+          <h2 className="text-gray-500">
             ¡Explorá nuestro catálogo de productos!
           </h2>
         </div>
-        <SearchBar buscar={handleBuscar} />
+        <div className="flex flex-col items-end mb-5">
+          <SearchBar buscar={handleBuscar} />
+        </div>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 p-6 w-full">
-        {lista.map((prod) => (
+      <SortedBy
+        type={type}
+        order={order}
+        onTypeSend={handleTypeData}
+        onOrderSend={handleOrderData}
+      />
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 my-5">
+        {sortedLista.map((prod) => (
           <ProductCard key={prod.id} producto={prod} />
         ))}
       </div>
 
       {destacados.length > 0 && (
-        <section className="mt-4 lg:mx-20">
-          <Carousel title="Productos Destacados" items={destacados} />
-        </section>
+        <Carousel title="Productos Destacados" items={destacados} />
       )}
-    </>
+    </div>
   );
 }
 
