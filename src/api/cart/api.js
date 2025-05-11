364ed6940e61722ae75d583d85015ceb33032c93
mquiss <mariaquispegamez@gmail.com>
2025-05-11 19:37:45 -0300

    feat: nuevas funciones para la api

--- src/api/cart/api.js
+++ src/api/cart/api.js
@@ -1,4 +1,15 @@
 import api from "../index";
 
+export const createCart = (userId) => api.post(`/carts/${userId}`, { products }); // TODO: ejecutar despues del register -> products vacio o con datos de local storage (si hay)
+
 export const fetchUserCart = (userId) => api.get(`/carts/${userId}`);
-export const saveUserCart = (userId, products) => api.put(`/carts/${userId}`, { products });
+
+export const saveUserCart = (userId, products) => api.put(`/carts/${userId}`, { products }); 
+
+export const updateProductQuantityInCart = (userId, productId, quantity) => { // TODO: implementar cuando esté endpoint en backend
+  return api.patch(`/carts/${userId}/products`, { productId, quantity });
+};
+
+export const addProductInCart = (userId, productData) => { // TODO: implementar cuando esté endpoint en backend
+  return api.post(`/carts/${userId}/products`, productData);
+};
