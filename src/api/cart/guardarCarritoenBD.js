2040dd332868b2afc438de239164ba10f775d972
mquiss <mariaquispegamez@gmail.com>
2025-05-13 19:54:19 -0300

    feat: función tipo servicio para crear carrito en db.json

--- src/api/cart/guardarCarritoenBD.js
+++ src/api/cart/guardarCarritoenBD.js
@@ -1,4 +1,4 @@
-import { saveUserCart } from "@src/api/cart/api";
+import { saveUserCart, createCart } from "@src/api/cart/api";
 
 export const guardarCarritoEnBD = async (userId, cartState) => {
   if (!userId) return;
@@ -12,5 +12,25 @@ export const guardarCarritoEnBD = async (userId, cartState) => {
     await saveUserCart(userId, products); 
   } catch (error) {
     console.error("Error al guardar el carrito en BD", error);
+  }
+};
+
+export const crearCarrito = async (userId, cartState) => {
+  if (!userId || !Array.isArray(cartState)) return;
+
+  const products = cartState.map((item) => ({
+    productId: item.productId,
+    quantity: item.quantity,
+  }));
+
+  const cartData = {
+    id: userId,
+    products: products,
+  };
+
+  try {
+    await createCart(cartData); 
+  } catch (error) {
+    console.error("Error al crear carrito en BD", error);
   }
 };
