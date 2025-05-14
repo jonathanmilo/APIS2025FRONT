64c45b0a06336d1d17a2e6d14871aa9f2a3eb14a
mquiss <mariaquispegamez@gmail.com>
2025-05-14 05:44:39 -0300

    style(mi-perfil): agregar colores dark mode

--- src/screens/MiPerfil/index.jsx
+++ src/screens/MiPerfil/index.jsx
@@ -43,23 +43,23 @@ const MiPerfil = () => {
           }}
         />
 
-        <p className="text-brand-black text-2xl">
+        <p className="text-black dark:text-white text-2xl">
           {user.firstName} {user.lastName}
         </p>
 
         <p className="text-gray-400">@{user.username}</p>
       </div>
 
-      <section className="bg-white m-5 lg:m-5 shadow-md p-5">
-        <div className="grid grid-cols-1 md:grid-cols-[1fr_auto] items-center gap-4  mb-5">
+      <section className="m-5 lg:m-5 p-5">
+        <div className="grid grid-cols-1 md:grid-cols-[1fr_auto] items-center gap-4 mb-5">
           <div className="flex gap-2 items-center justify-center lg:justify-start">
-            <p className="text-brand-black uppercase lg:text-xl">
+            <p className="text-black dark:text-white uppercase lg:text-xl">
               Mis publicaciones
             </p>
             <Tooltip title="Nueva publicación" arrow>
               <button
                 onClick={() => navigate("/vender")}
-                className="border-2 border-brand-main lg:p-1 text-xl text-brand-main hover:bg-gray-100 cursor-pointer"
+                className="border-2 rounded-full border-primary lg:p-1 text-xl text-primary hover:bg-gray-100 dark:hover:bg-black cursor-pointer"
               >
                 <FaPlus />
               </button>
