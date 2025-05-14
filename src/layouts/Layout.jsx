0b7ba0e08bf0d5038265be3654c9b7b719388f7a
mquiss <mariaquispegamez@gmail.com>
2025-05-14 05:45:18 -0300

    style(layout): agregar colores dark mode

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -6,7 +6,7 @@ export default function Layout() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow bg-white dark:bg-gray-900 min-h-screen transition-colors duration-300">
+      <main className="flex-grow bg-gray-50 dark:bg-[color:var(--color-gray)] min-h-screen transition-colors duration-300">
         <Outlet />
       </main>
       <Footer />
