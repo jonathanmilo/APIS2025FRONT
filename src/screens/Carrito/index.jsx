86d66ee3a0590842378e091ad3d273cb7e1280a7
mquiss <mariaquispegamez@gmail.com>
2025-05-14 05:42:18 -0300

    style(carrito): agregar colores dark mode

--- src/screens/Carrito/index.jsx
+++ src/screens/Carrito/index.jsx
@@ -52,14 +52,14 @@ export default function Carrito() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 pt-10 px-4">
-      <h1 className="mb-5 text-xl text-center text-brand-black uppercase font-bold">
+    <div className="min-h-screen pt-10 px-4">
+      <h1 className="mb-5 text-xl text-center text-black dark:text-white uppercase font-bold">
         Carrito de Compras
       </h1>
       <div className="mx-auto max-w-5xl justify-center md:flex md:space-x-6 xl:px-0">
         <div className="rounded-lg md:w-2/3">
           {cart.length === 0 ? (
-            <p className="text-brand-gray text-center py-6 bg-white shadow-md">
+            <p className="text-black dark:text-white text-center py-6 bg-white dark:bg-black shadow-md">
               El carrito está vacío.
             </p>
           ) : (
@@ -68,7 +68,7 @@ export default function Carrito() {
               return (
                 <div
                   key={item.productId}
-                  className="mb-6 flex flex-col justify-between bg-white p-6 shadow-md sm:flex-row sm:justify-start"
+                  className="mb-6 flex flex-col justify-between bg-white dark:bg-black p-6 shadow-md sm:flex-row sm:justify-start"
                 >
                   <img
                     src={producto.images[0]?.url}
@@ -76,11 +76,11 @@ export default function Carrito() {
                     onError={(e) => {
                       e.target.src = "/placeholder.jpg";
                     }}
-                    className="h-40 sm:w-40 object-contain"
+                    className="h-40 w-40 object-contain self-center"
                   />
                   <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
                     <div className="mt-5 sm:mt-0">
-                      <h2 className="text-lg font-bold text-brand-black dark:text-white">
+                      <h2 className="text-lg font-bold text-black dark:text-white">
                         {producto.title}
                       </h2>
                       <p className="mt-1 text-sm text-gray-700 dark:text-gray-300">
@@ -94,13 +94,13 @@ export default function Carrito() {
                             updateQuantity(item.productId, item.quantity - 1)
                           }
                           disabled={item.quantity <= 1}
-                          className="cursor-pointer rounded-l bg-gray-100 text-brand-black py-1 px-3.5 duration-100 hover:bg-brand-main hover:text-white disabled:opacity-50"
+                          className="cursor-pointer rounded-l bg-gray-100 text-black py-1 px-3.5 duration-100 hover:bg-primary hover:text-white disabled:opacity-50"
                         >
                           -
                         </button>
 
                         <input
-                          className="h-8 w-8 bg-white text-brand-black text-center  outline-none"
+                          className="h-8 w-8 bg-white text-black text-center outline-none"
                           type="number"
                           value={item.quantity}
                           min="1"
@@ -110,26 +110,26 @@ export default function Carrito() {
                           onClick={() =>
                             updateQuantity(item.productId, item.quantity + 1)
                           }
-                          className="cursor-pointer rounded-r bg-gray-100 text-brand-black  py-1 px-3 duration-100 hover:bg-brand-main hover:text-white"
+                          className="cursor-pointer rounded-r bg-gray-100 text-black py-1 px-3 duration-100 hover:bg-primary hover:text-white"
                         >
                           +
                         </button>
                       </div>
                       <div className="flex items-center space-x-4">
-                        <p className="text-sm font-semibold text-gray-700">
+                        <p className="text-sm font-semibold text-black dark:text-white">
                           $
                           {calcularPrecio(
                             producto.price,
                             producto.discountPercentage
                           ).toFixed(2)}
                         </p>
-                        <Tooltip title="Eliminar">
+                        <Tooltip title="Eliminar" arrow>
                           <IconButton
                             onClick={() => {
                               sonidito(), removeFromCart(item.productId);
                             }}
                           >
-                            <RiDeleteBin6Line />
+                            <RiDeleteBin6Line className="text-black dark:text-white"/>
                           </IconButton>
                         </Tooltip>
                       </div>
@@ -143,10 +143,10 @@ export default function Carrito() {
 
         {/* Resumen de compra */}
         {cart.length > 0 && (
-          <div className="mt-6 h-full  border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
+          <div className="mt-6 h-full bg-white dark:bg-black p-6 shadow-md md:mt-0 md:w-1/3">
             <div className="mb-2 flex justify-between">
-              <p className="text-gray-700">Subtotal</p>
-              <p className="text-gray-700">
+              <p className="text-black dark:text-white">Subtotal</p>
+              <p className="text-black dark:text-white">
                 $
                 {cart
                   .reduce((sum, item) => {
@@ -158,8 +158,8 @@ export default function Carrito() {
             </div>
             {cart.some((item) => item.productData?.discountPercentage > 0) && (
               <div className="mb-2 flex justify-between">
-                <p className="text-gray-700">Descuentos</p>
-                <p className="text-gray-700">
+                <p className="text-black dark:text-white">Descuentos</p>
+                <p className="text-black dark:text-white">
                   -$
                   {cart
                     .reduce((sum, item) => {
@@ -172,14 +172,14 @@ export default function Carrito() {
                 </p>
               </div>
             )}
-            <hr className="my-4" />
+            <hr className="my-4 text-black dark:text-white" />
             <div className="flex justify-between">
-              <p className="text-lg font-bold text-brand-black">Total</p>
+              <p className="text-lg font-bold text-black dark:text-white">Total</p>
               <div className="flex flex-col items-end">
-                <p className="mb-1 text-lg font-bold text-brand-black">
+                <p className="mb-1 text-lg font-bold text-black dark:text-white">
                   ${calcularTotal().toFixed(2)} USD
                 </p>
-                <p className="text-xs text-gray-700">Incluye impuestos</p>
+                <p className="text-xs text-gray-700 dark:text-white">Incluye impuestos</p>
               </div>
             </div>
 
@@ -187,7 +187,7 @@ export default function Carrito() {
 
             <button
               onClick={handleCheckout}
-              className="mt-6 w-full rounded-md bg-brand-main py-2 font-medium text-white hover:bg-brand-main-hover"
+              className="mt-6 w-full rounded-md bg-primary py-2 font-medium text-white hover:bg-primary-hover"
             >
               Finalizar Compra
             </button>
