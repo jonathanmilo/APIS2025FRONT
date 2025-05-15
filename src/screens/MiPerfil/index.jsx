b3ca29ee2ea536ffb9651619092b8b016cc70483
mquiss <mariaquispegamez@gmail.com>
2025-05-14 23:14:00 -0300

    style(mi-perfil): eliminar parte superior de usuario y modificar className de section publicaciones

--- src/screens/MiPerfil/index.jsx
+++ src/screens/MiPerfil/index.jsx
@@ -7,9 +7,7 @@ import { FaPlus } from "react-icons/fa6";
 import { useState, useEffect, useCallback } from "react";
 import { fetchUserProducts } from "@src/api/products.js";
 
-import {
-  filtrarPorNombre,
-} from "@src/utils/filtrarProductos";
+import { filtrarPorNombre } from "@src/utils/filtrarProductos";
 import SearchBar from "@src/components/SearchBar";
 
 const MiPerfil = () => {
@@ -32,7 +30,10 @@ const MiPerfil = () => {
         // Si hay productos filtrados, actualizar también esa lista
         if (productosFiltrados.length > 0) {
           setProductosFiltrados(
-            filtrarPorNombre(response.data, document.querySelector('input[type="search"]')?.value || '')
+            filtrarPorNombre(
+              response.data,
+              document.querySelector('input[type="search"]')?.value || ""
+            )
           );
         }
         setLoading(false);
@@ -51,83 +52,60 @@ const MiPerfil = () => {
 
   const handleRemoveProduct = (productId) => {
     // Actualizar la lista de productos del usuario
-    setProductosUsuario(prevProducts => 
-      prevProducts.filter(product => product.id !== productId)
+    setProductosUsuario((prevProducts) =>
+      prevProducts.filter((product) => product.id !== productId)
     );
-    
+
     // Actualizar la lista de productos filtrados si es necesario
     if (productosFiltrados.length > 0) {
-      setProductosFiltrados(prevFiltered => 
-        prevFiltered.filter(product => product.id !== productId)
+      setProductosFiltrados((prevFiltered) =>
+        prevFiltered.filter((product) => product.id !== productId)
       );
     }
   };
 
   const handleBuscar = (termino) => {
     if (!termino) return setProductosFiltrados([]);
-    setProductosFiltrados(
-      filtrarPorNombre(productosUsuario, termino)
-    );
+    setProductosFiltrados(filtrarPorNombre(productosUsuario, termino));
   };
 
   if (!user) return null;
 
   return (
-    <>
-      <div className="flex flex-col justify-center items-center pt-5">
-        <Avatar
-          src={user.avatar}
-          alt={user?.firstName || "User"}
-          sx={{
-            width: 80,
-            height: 80,
-          }}
-        />
-
-        <p className="text-black dark:text-white text-2xl">
-          {user.firstName} {user.lastName}
-        </p>
-
-        <p className="text-gray-400">@{user.username}</p>
-      </div>
-
-      <section className="m-5 lg:m-5 p-5">
-        <div className="grid grid-cols-1 md:grid-cols-[1fr_auto] items-center gap-4 mb-5">
-          <div className="flex gap-2 items-center justify-center lg:justify-start">
-            <p className="text-black dark:text-white uppercase lg:text-xl">
-              Mis publicaciones
-            </p>
-            <Tooltip title="Nueva publicación" arrow>
-              <button
-                onClick={() => navigate("/vender")}
-                className="border-2 rounded-full border-primary lg:p-1 text-xl text-primary hover:bg-gray-100 dark:hover:bg-black cursor-pointer"
-              >
-                <FaPlus />
-              </button>
-            </Tooltip>
-          </div>
-
-          <SearchBar buscar={handleBuscar} />
+    <section className="m-5 lg:m-5 p-5 min-h-screen">
+      <div className="grid grid-cols-1 sm:grid-cols-[1fr_auto] items-center gap-4 mb-5">
+        <div className="flex gap-3 items-center justify-center sm:justify-start">
+          <p className="text-2xl font-bold text-black dark:text-white uppercase">
+            Mis publicaciones
+          </p>
+          <Tooltip title="Nueva publicación" arrow>
+            <button
+              onClick={() => navigate("/vender")}
+              className="border-2 rounded-full border-primary p-1 text-xl text-primary hover:bg-gray-100 dark:hover:bg-black cursor-pointer"
+            >
+              <FaPlus />
+            </button>
+          </Tooltip>
         </div>
 
-        {loading ? (
-          <p className="text-center">Cargando productos...</p>
-        ) : error ? (
-          <p className="text-center text-red-500">{error}</p>
-        ) : (
-          <ListaProductos
-            titulo={"Mis publicaciones"}
-            productos={
-              productosFiltrados.length
-                ? productosFiltrados
-                : productosUsuario
-            }
-            onRemoveProduct={handleRemoveProduct}
-            onUpdateStock={cargarProductosUsuario}
-          />
-        )}
-      </section>
-    </>
+        <div className="flex items-center justify-center"><SearchBar buscar={handleBuscar} /></div>
+      </div>
+
+      {loading ? (
+        <p className="text-center">Cargando productos...</p>
+      ) : error ? (
+        <p className="text-center text-red-500">{error}</p>
+      ) : (
+        <ListaProductos
+          titulo={"Mis publicaciones"}
+          productos={
+            productosFiltrados.length ? productosFiltrados : productosUsuario
+          }
+          onRemoveProduct={handleRemoveProduct}
+          onUpdateStock={cargarProductosUsuario}
+        />
+      )}
+    </section>
   );
 };
 
