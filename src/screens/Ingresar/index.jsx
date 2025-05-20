609c81fbde3498b70194a38ee316e2615b4e920d
mquiss <mariaquispegamez@gmail.com>
2025-05-20 15:46:30 -0300

    feat(ingresar): implementar formularios en index.jsx y delegar tarea de cargar carrito a CartContext.jsx

--- src/screens/Ingresar/index.jsx
+++ src/screens/Ingresar/index.jsx
@@ -1,39 +1,9 @@
-import { useState, useContext, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import AuthForm from "./components/AuthForm";
-import { useValidacion } from "@src/contexts/AuthContext";
-import { CartContext } from "@src/contexts/CartContext";
-import { crearCarrito } from "@src/api/cart/cartService";
+import { useState } from "react";
+import RegisterForm from "./components/RegisterForm";
+import LoginForm from "./components/LoginForm";
 
 export default function Ingresar() {
-  const { cart } = useContext(CartContext);
-  const { validar, user, register } = useValidacion();
-  const { obtenerCarrito } = useContext(CartContext);
-  const navigate = useNavigate();
   const [formMode, setFormMode] = useState("login");
-  const [loginExitoso, setLoginExitoso] = useState(false);
-
-  const handleSubmit = async (data) => {
-    if (formMode === "login") {
-      await validar(data);
-      setLoginExitoso(true);
-    } else {
-      const success = await register(data);
-
-      if (success) {
-        await crearCarrito(data.id, cart);
-        setFormMode("login");
-      }
-    }
-  };
-
-  useEffect(() => {
-    if (loginExitoso && user) {
-      obtenerCarrito();
-      alert(`Bienvenido ${user.name || "usuario"}!`);
-      navigate("/");
-    }
-  }, [loginExitoso, user]);
 
   const toggleFormMode = () => {
     setFormMode((prev) => (prev === "login" ? "register" : "login"));
@@ -43,7 +13,12 @@ export default function Ingresar() {
   return (
     <div className="flex-grow flex flex-col items-center min-h-screen p-10">
       <div className="relative flex flex-col bg-transparent text-black">
-        <AuthForm mode={formMode} onSubmit={handleSubmit} />
+
+        {formMode === "login" ? (
+          <LoginForm />
+        ) : (
+          <RegisterForm setFormMode={setFormMode} />
+        )}
 
         <div className="flex items-center justify-center w-full gap-4 my-6 px-4">
           <div className="h-px flex-1 bg-gray-300 dark:text-white" />
