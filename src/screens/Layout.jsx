4382ec0f44364f15e1d381c2cb77188a3bbc0547
mquiss <mariaquispegamez@gmail.com>
2025-06-10 23:22:50 -0300

    style: cambiar color de fondo light mode a variable

--- src/screens/Layout.jsx
+++ src/screens/Layout.jsx
@@ -6,7 +6,7 @@ export default function Layout() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow bg-gray-50 dark:bg-[color:var(--color-gray)] min-h-screen transition-colors duration-300">
+      <main className="flex-grow bg-[color:var(--color-gray)] dark:bg-[color:var(--color-gray)] min-h-screen transition-colors duration-300">
         <Outlet />
       </main>
       <Footer />
